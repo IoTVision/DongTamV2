d2e750ca8ee166827f5cc2caba0e2fd2cd6ecf63
TM-khoa <minhkhoa9a31314@gmail.com>
2023-04-19 15:21:21 +0700

    First test GUI successfully

--- Code/ESP32/main/PressureIndicator.c
+++ Code/ESP32/main/PressureIndicator.c
@@ -1,55 +1,23 @@
 #include "74HC595.h"
+#include "esp_log.h"
+#ifndef PRESSURE_INDICATOR
+#define PRESSURE_INDICATOR
 #define INDICATOR_MAX_BITMASK 0x03ff
 #define INDICATOR_MIN_BITMASK 0x0200
 #define INDICATOR_MAX_LEVEL 10
 
-#define HC595_OE_MASK (1ULL<<GPIO_NUM_4)
-#define HC595_LATCH_MASK (1ULL<<GPIO_NUM_5)
-#define HC595_CLK_MASK (1ULL<<GPIO_NUM_18)
-#define HC595_DS_MASK (1ULL<<GPIO_NUM_23)
-
-HC595 hc595_pi;
 
 void PI_SetLevel(uint8_t level){
     if(level > 10) return;
     uint16_t indicator = INDICATOR_MAX_BITMASK;
     indicator <<= (INDICATOR_MAX_LEVEL-level);
     indicator &=INDICATOR_MAX_BITMASK;
-    uint8_t data[2] = {data[0]=indicator >> 8,data[1]=indicator,};
+    uint8_t data[2] = {0};
+    data[0]=indicator;
+    data[1]=indicator >> 8;
     HC595_Send_Data(data,2);
 }
 
-void PI_ConfigPin()
-{
-    gpio_config_t cfg = {
-        .intr_type = GPIO_INTR_DISABLE,
-        .mode = GPIO_MODE_OUTPUT,
-        .pin_bit_mask = HC595_DS_MASK |
-                        HC595_OE_MASK |
-                        HC595_CLK_MASK|
-                        HC595_LATCH_MASK,
-        .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .pull_up_en = GPIO_PULLUP_DISABLE,
-    };
-    gpio_config(&cfg);
-    HC595_AssignPin(&hc595_pi,GPIO_NUM_5,HC595_LATCH);
-    HC595_AssignPin(&hc595_pi,GPIO_NUM_4,HC595_OE);
-    HC595_AssignPin(&hc595_pi,GPIO_NUM_18,HC595_CLK);
-    HC595_AssignPin(&hc595_pi,GPIO_NUM_23,HC595_DS);
-    HC595_SetTarget(&hc595_pi);
-    HC595_Enable();
-    
-    PI_SetLevel(1);
-    vTaskDelay(500/portTICK_PERIOD_MS);
-    PI_SetLevel(3);
-    vTaskDelay(500/portTICK_PERIOD_MS);
-    PI_SetLevel(5);
-    vTaskDelay(500/portTICK_PERIOD_MS);
-    PI_SetLevel(7);
-    vTaskDelay(500/portTICK_PERIOD_MS);
-    PI_SetLevel(9);
-    vTaskDelay(500/portTICK_PERIOD_MS);
-}
 void PI_TestShowLevel_Increase(){
     static uint16_t dataOriginal= INDICATOR_MIN_BITMASK;
     static uint8_t shft=0;
@@ -61,8 +29,8 @@ void PI_TestShowLevel_Increase(){
         shft = 0;
         dataOriginal = INDICATOR_MIN_BITMASK;
     }
-    data[0]=dataOriginal >> 8;
-    data[1]=dataOriginal;
+    data[1]=dataOriginal >> 8;
+    data[0]=dataOriginal;
     HC595_Send_Data(data,2);
     DELAY_MS(50);
 }
@@ -77,9 +45,27 @@ void PI_TestShowLevel_Decrease(){
         shft = 0;
         dataOriginal = INDICATOR_MAX_BITMASK;
     }
-    data[0]=dataOriginal >> 8;
-    data[1]=dataOriginal;
+    data[1]=dataOriginal >> 8;
+    data[0]=dataOriginal;
     HC595_Send_Data(data,2);
     DELAY_MS(50);
 }
 
+void PI_Test()
+{
+    PI_SetLevel(2);
+    vTaskDelay(500/portTICK_PERIOD_MS);
+    PI_SetLevel(4);
+    vTaskDelay(500/portTICK_PERIOD_MS);
+    PI_SetLevel(6);
+    vTaskDelay(500/portTICK_PERIOD_MS);
+    PI_SetLevel(8);
+    vTaskDelay(500/portTICK_PERIOD_MS);
+    PI_SetLevel(10);
+    vTaskDelay(500/portTICK_PERIOD_MS);
+    
+    for(uint8_t i=0;i<10;i++)PI_TestShowLevel_Increase();
+    for(uint8_t i=0;i<10;i++)PI_TestShowLevel_Decrease();
+    PI_SetLevel(0);
+}
+#endif
