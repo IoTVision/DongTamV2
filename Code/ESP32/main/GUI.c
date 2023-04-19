0fc359086d3c7b5d54a6c97863474de15001d47c
TM-khoa <minhkhoa9a31314@gmail.com>
2023-04-19 15:21:21 +0700

    First test GUI successfully

---
+++ Code/ESP32/main/GUI.c
@@ -0,0 +1,36 @@
+#ifndef GUI
+#define GUI
+#include "esp_err.h"
+#include "i2cdev.h"
+#include "LCD_I2C.h"
+#include "esp_check.h"
+#include "PressureIndicator.c"
+#include "74HC595.h"
+LCDI2C lcdI2C;
+
+#define BTN_MODE GPIO_NUM_35
+#define BTN_SET GPIO_NUM_34
+#define BTN_UP GPIO_NUM_5
+#define BTN_DOWN_RIGHT GPIO_NUM_4
+#define LED_ERROR_MASK (1U << 14)
+#define LED_STATUS_MASK (1U << 15)
+
+esp_err_t LCD_init()
+{
+    esp_err_t err = ESP_OK;
+    err = LCDI2C_Config(&lcdI2C);
+    LCDI2C_TurnOnBackLight();
+    LCDI2C_Print("SpiritBoi",0,0);
+    return err;
+}
+
+void PressureIndicator_Test()
+{
+    PI_Test();
+}
+
+void Button_Test()
+{
+
+}
+#endif
--- Code/ESP32/main/main.c
+++ Code/ESP32/main/main.c
@@ -5,28 +5,59 @@
 #include "PressureIndicator.c"
 #include "UART.c"
 #include "driver/dac.h"
+#include "esp_err.h"
+#include "GUI.c"
+#include "LCD_I2C.h"
 // static const char *TAG= "main";
 
-void PressureIndicator_Init();
+#define HC595_OE_MASK (1ULL<<GPIO_NUM_4)
+#define HC595_LATCH_MASK (1ULL<<GPIO_NUM_5)
+#define HC595_CLK_MASK (1ULL<<GPIO_NUM_18)
+#define HC595_DS_MASK (1ULL<<GPIO_NUM_23)
+
+HC595 hc595_pi;//74HC595 for Pressure Indicator
+
+void HC595_ConfigPin();
+
+
 
 void app_main(void)
 {
+    HC595_ConfigPin();
+    PressureIndicator_Test();
     UARTConfig();
-    PressureIndicator_Init();
     xTaskCreate(UARTToSTM32_event_task, "UARTToSTM32_event_task", 2048, NULL, 4, NULL);
     xTaskCreate(log_uart_event_task, "log_uart_event_task", 2048, NULL, 4, NULL);
     while (1) {
-    vTaskDelay(1000/portTICK_PERIOD_MS);
+        vTaskDelay(2000/portTICK_PERIOD_MS);
     }
 }
 
-void PressureIndicator_Init()
+
+void HC595_ConfigPin()
 {
-    PI_ConfigPin();
-    for(uint8_t i=0;i<10;i++)PI_TestShowLevel_Increase();
-    for(uint8_t i=0;i<10;i++)PI_TestShowLevel_Decrease();
-    PI_SetLevel(0);
+    gpio_config_t cfg = {
+        .intr_type = GPIO_INTR_DISABLE,
+        .mode = GPIO_MODE_OUTPUT,
+        .pin_bit_mask = HC595_DS_MASK |
+                        HC595_OE_MASK |
+                        HC595_CLK_MASK|
+                        HC595_LATCH_MASK,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+    };
+    gpio_config(&cfg);
+    HC595_AssignPin(&hc595_pi,GPIO_NUM_5,HC595_LATCH);
+    HC595_AssignPin(&hc595_pi,GPIO_NUM_4,HC595_OE);
+    HC595_AssignPin(&hc595_pi,GPIO_NUM_18,HC595_CLK);
+    HC595_AssignPin(&hc595_pi,GPIO_NUM_23,HC595_DS);
+    HC595_SetTarget(&hc595_pi);
+    HC595_Enable();
 }
+
+
+
+
 
 
 
